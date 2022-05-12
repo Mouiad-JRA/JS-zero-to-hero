94ad60dfb9751055ce373cc4b4675c3d2914829c
Mouiad JRA <mouiad.alilo@gmail.com>
2022-05-12 12:46:43 +0300

    WIP : Finish model window project

--- 06-Model/script.js
+++ 06-Model/script.js
@@ -1,1 +1,26 @@
 "use strict";
+
+const modal = document.querySelector(".modal");
+const btnCloseModel = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+const btnOpenModal = document.querySelectorAll(".show-modal");
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+const openModel = function () {
+  modal.classList.remove("hidden"); // equal to modal.style.display = 'block' only work for one attr
+  overlay.classList.remove("hidden");
+};
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener("click", openModel); // not calling openModel , just declare it (defined it)
+}
+
+overlay.addEventListener("click", closeModal);
+btnCloseModel.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
