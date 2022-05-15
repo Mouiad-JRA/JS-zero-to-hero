71113e3f3b00e3c2b1095d22ec4f6843c31690c1
Mouiad JRA <mouiad.alilo@gmail.com>
2022-05-15 08:27:13 +0300

    update: add roles.

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -12,6 +12,12 @@ const btnRoll = document.querySelector(".btn--roll");
 let current0El = document.getElementById("current--0");
 let current1El = document.getElementById("current--1");
 
+
+const modal = document.querySelector(".modal");
+const btnCloseModel = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+const btnOpenModal = document.querySelector(".show-modal");
+
 let scores, currentScore, activePlayer, playing;
 // Start Conditions
 const init = function () {
@@ -89,3 +95,25 @@ btnHold.addEventListener("click", function () {
 });
 
 btnNew.addEventListener("click", init);
+
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+const openModel = function () {
+  modal.classList.remove("hidden"); // equal to modal.style.display = 'block' only work for one attr
+  overlay.classList.remove("hidden");
+};
+
+btnOpenModal.addEventListener("click", openModel); // not calling openModel , just declare it (defined it)
+
+
+overlay.addEventListener("click", closeModal);
+btnCloseModel.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
