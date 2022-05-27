fff68548defc36c9196dcda46dd6bd4decb7bd3b
Mouiad JRA <mouiad.alilo@gmail.com>
2022-05-27 19:19:12 +0300

    update: finish Challenge #4

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -89,4 +89,150 @@ const calcAverageHumanAge = (ages) =>{
     .reduce((acc, age, arr) => acc + age / arr.length, 0);
 };
 
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
 
+///////////////////////////////////////
+// Coding Challenge #4
+
+/*
+Julia and Kate are still studying dogs, and this time they are studying if dogs are eating too much or too little.
+Eating too much means the dog's current food portion is larger than the recommended portion, and eating too little is the opposite.
+Eating an okay amount means the dog's current food portion is within a range 10% above and 10% below the recommended portion (see hint).
+1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion and
+add it to the object as a new property. Do NOT create a new array, simply loop over the array.
+ Forumla: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
+ */
+dogs.map((dog)=> dog.recommended = dog.weight ** 0.75 * 28)
+console.log(`solution for Q One:`);
+console.log(dogs);
+/*
+2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple owners,
+ so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
+
+ const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+ */
+const sarahDog = dogs.find(dog=> dog.owners.includes("Sarah"))
+
+// if (sarahDog.curFood > (sarahDog.recommended )){
+//   results = `Sarah Dog is eating too much`
+// } else if (sarahDog.curFood < (sarahDog.recommended )) {
+//   results = `Sarah Dog is eating too little`
+// } else results = `Sarah Dog is normal`;
+// console.log(`solution for Q Two:`);
+// console.log(results);
+
+
+/*
+3. Create an array containing all owners of dogs who eat too much ('ownersEatTooMuch')
+and an array with all owners of dogs who eat too little ('ownersEatTooLittle').
+ const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+*/
+const ownersEatTooMuch = dogs.filter(dog=>dog.curFood>
+    (dog.recommended)).flatMap(el=>el.owners);
+const ownersEatTooLittle = dogs.filter(dog=>dog.curFood <
+    (dog.recommended)).flatMap(el=>el.owners);
+console.log(`solution for Q Three:`);
+console.log(ownersEatTooMuch)
+console.log(ownersEatTooLittle)
+/*
+4. Log a string to the console for each array created in 3.,
+like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
+ const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+*/
+
+console.log(`solution for Q Four:`);
+console.log(ownersEatTooMuch.join(' and ')+`'s dogs eat too much!`);
+console.log(ownersEatTooLittle.join(' and ')+`'s dogs eat too Little!`);
+/*
+5. Log to the console whether there is any dog eating EXACTLY the amount of food that is recommended (just true or false)
+ const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+*/
+console.log(`solution for Q Five:`);
+console.log(dogs.some(dog=>dog.curFood===dog.recommended));
+console.log(`There is no any dog with the recommended food `)
+/*
+6. Log to the console whether there is any dog eating an OKAY amount of food (just true or false)
+ const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+*/
+console.log(`solution for Q Six:`);
+console.log(dogs.some(dog=>dog.curFood > (dog.recommended*0.9) && dog.curFood < (dog.recommended*1.1)));
+console.log(`There is dog with an OKAY amount of food `)
+/*
+7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
+ const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+*/
+const dogWithOkayAmount = dogs.filter(dog=>dog.curFood >
+    (dog.recommended*0.9) && dog.curFood < (dog.recommended*1.1));
+console.log(`solution for Q Seven:`);
+console.log(dogWithOkayAmount);
+/*
+8. Create a shallow copy of the dogs array and sort it by recommended food portion
+in an ascending order (keep in mind that the portions are inside the array's objects)
+ const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+*/
+console.log(`solution for Q eight:`);
+const copy = dogs.slice().sort((a,b)=>a.recommended-b.recommended);
+console.log(copy);
+/*
+HINT 1: Use many different tools to solve these challenges, you can use the summary lecture to choose between them 😉
+HINT 2: Being within a range 10% above and below the recommended portion means:
+current > (recommended * 0.90) && current < (recommended * 1.10).
+ Basically, the current portion should be between 90% and 110% of the recommended portion.
+TEST DATA:
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] }
+];
+GOOD LUCK 😀
+*/
+
+/*
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+*/
