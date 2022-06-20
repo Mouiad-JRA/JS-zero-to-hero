026843ce594ac8ac5004d29cebc6986db2128378
Mouiad JRA <mouiad.alilo@gmail.com>
2022-06-20 21:17:27 +0300

    update: finish Challenge #2 in 14

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -34,3 +34,47 @@ console.log(car1.make , ' going at', car1.speed, "km/h" );
 const car2 = new Car('Mercedes', 120);
 car2.accelerate()
 console.log(car2.make , ' going at', car2.speed, "km/h");
+
+
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/*
+1. Re-create challenge 1, but this time using an ES6 class;
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value,
+ by multiplying the input by 1.6);
+4. Create a new car and experiment with to accelerate and brake methods, and with the getter and setter.
+DATA CAR 1: 'Ford' going at 120 km/h
+GOOD LUCK 😀
+*/
+
+
+class CarCl{
+    constructor(make, speed) {
+        this.make = make;
+        this.speed = speed;
+    }
+    get speedUS(){
+        return this.speed /1.6;
+    }
+    set speedUS(speed){
+        this.speed = this.speed *1.6;
+    }
+    brake (){
+        this.speed -=5;
+        console.log(this.speed);
+        return this.speed;
+    }
+    accelerate(){
+        this.speed += 10;
+        console.log(this.speed);
+        return this.speed;
+    }
+}
+
+const car = new CarCl("BMW", 120);
+car.accelerate();
+car.brake();
+console.log(car.speedUS);
